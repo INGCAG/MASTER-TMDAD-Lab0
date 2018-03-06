4a37164dd6598c5ab8a6ae2c03a91f1aa39cad0a
INGCAG <ingcaguayaral@gmail.com>
2018-03-06 22:44:25 +0100

    Lab0: Add Restful methods

--- src/main/java/es/unizar/tmdad/lab0/controller/SearchController.java
+++ src/main/java/es/unizar/tmdad/lab0/controller/SearchController.java
@@ -6,10 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.social.UncategorizedApiException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
